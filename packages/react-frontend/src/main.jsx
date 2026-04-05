8d966e7e9ca874e13625504d463d0de64c971ba8
FloatRun <pranavdumpa@gmail.com>
2026-04-04 21:58:37 -0700

    implemented state

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import MyApp from "./MyApp" // file's extension converted to js regardless by Vite
+import Table from "./Table"
+import "./main.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// create the actual container
+const container = document.getElementById("root");
+
+// create a root
+const root = ReactDOMClient.createRoot(container);
+
+// Initial render
+root.render(<MyApp />);
