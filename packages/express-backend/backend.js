dfcdbd1b2d7bd5b208f137d6f9e976a68d591ce2
FloatRun <pranavdumpa@gmail.com>
2026-04-12 19:11:03 -0700

    added user id filter logic

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -22,9 +22,28 @@ app.get("/users", (req, res) => {
     }
 })
 
+app.get("/users/:id", (req, res) => {
+    const id = req.params.id
+    let result = findUserById(id)
+    if (result === undefined) {
+        res.status(404).send("Resource not found.")
+    }
+    else {
+        res.send(result)
+    }
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on http://localhost:${port}`);
 })
+
+const findUserById = (id) => {
+    return users.users_list.find((user) => user.id === id);
+}
+
+const findUserByName = (name) => {
+    return users.users_list.filter((user) => user.name === name);
+}
 
 const users = {
   users_list: [
@@ -55,9 +74,5 @@ const users = {
     }
   ]
 };
-
-const findUserByName = (name) => {
-    return users.users_list.filter((user) => user.name === name);
-}
 
 // Setting debugger: export DEBUG='express:router'
